77a29c3b078d886685496fff18e6617817e1f8a2
amishapatel00 <amishapatel9882@gmail.com>
2026-09-03 12:07:03 +0530

    Initial commit

---
+++ Backend/config/db.js
@@ -0,0 +1,12 @@
+      import mongoose from "mongoose";    
+      const connectDB=async()=>{
+          try{
+                    await mongoose.connect(process.env.MONGODB_URI);
+                    console.log("Mongodb connected successfully");
+          }
+          catch(error){
+                    console.error(`Error:${error.message}`);
+
+          }
+      }
+       export default connectDB;   
